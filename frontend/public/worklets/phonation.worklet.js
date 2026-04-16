5a90997c0bd5f3cf1a0a37721bd5e2af470168eb
Dayan <dayancorvo@gmail.com>
2026-04-16 02:40:35 -0400

    Add phonation worklet processor skeleton with comments

---
+++ frontend/public/worklets/phonation.worklet.js
@@ -0,0 +1,23 @@
+// public/worklets/phonation.worklet.js
+
+/*
+Clase PhonationProcessor que extiende AudioWorkletProcessor.
+Método process(inputs, outputs, parameters) vacío que devuelve true.
+Constructor con this.port.onmessage vacío.
+registerProcessor('phonation-processor', PhonationProcessor) al final.
+Todo el código comentado.
+*/
+
+// class PhonationProcessor extends AudioWorkletProcessor {
+//   constructor() {
+//     super();
+//     this.port.onmessage = (event) => {
+//       // Sin lógica por ahora
+//     };
+//   }
+//   process(inputs, outputs, parameters) {
+//     // Procesamiento de audio (a implementar)
+//     return true;
+//   }
+// }
+// registerProcessor('phonation-processor', PhonationProcessor);
