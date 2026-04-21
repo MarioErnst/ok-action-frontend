758e6b1e085f5cb0fb03ea5a0d442f3a92f1d460
Dayan <dayancorvo@gmail.com>
2026-04-21 00:44:43 -0400

    feat: add CoachMessage component for displaying feedback based on loudness levels

--- frontend/src/features/loudness/index.ts
+++ frontend/src/features/loudness/index.ts
@@ -4,3 +4,4 @@ export { LOUDNESS_PRESETS, getPresetById } from './services/loudnessPresets';
 export { default as useLoudnessCoach } from './hooks/useLoudnessCoach';
 export { default as BandLabel } from './components/atoms/BandLabel';
 export { default as LoudnessMeter } from './components/molecules/LoudnessMeter';
+export { default as CoachMessage } from './components/molecules/CoachMessage';
