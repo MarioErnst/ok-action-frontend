1fc081c7fa0fbe5804ff745f41315c04976fb2e2
Dayan <dayancorvo@gmail.com>
2026-04-21 00:39:03 -0400

    feat: add BandLabel and LoudnessMeter components for visualizing loudness metrics

--- frontend/src/features/loudness/index.ts
+++ frontend/src/features/loudness/index.ts
@@ -2,3 +2,5 @@ export type { LoudnessBand, LoudnessConfig, LoudnessMetrics } from './types';
 export { classifyLoudness } from './services/loudnessClassifier';
 export { LOUDNESS_PRESETS, getPresetById } from './services/loudnessPresets';
 export { default as useLoudnessCoach } from './hooks/useLoudnessCoach';
+export { default as BandLabel } from './components/atoms/BandLabel';
+export { default as LoudnessMeter } from './components/molecules/LoudnessMeter';
