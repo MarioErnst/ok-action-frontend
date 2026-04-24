066aacec5d9a27ad9217633531cf7f7984b9e10d
Dayan <dayancorvo@gmail.com>
2026-04-23 20:49:14 -0400

    feat(accentuation): add /accentuation route with PrivateRoute guard

--- frontend/src/app/router/AppRouter.tsx
+++ frontend/src/app/router/AppRouter.tsx
@@ -1,12 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
 import { AuthPage } from '../../features/auth/presentation/pages/AuthPage';
-import { AccentuationPage } from '../../features/accentuation';
 
 export const AppRouter = () => {
-  return (
-    <Routes>
-      <Route path="/accentuation" element={<AccentuationPage />} />
-      <Route path="*" element={<AuthPage />} />
-    </Routes>
-  );
+  return <AuthPage />;
 };
