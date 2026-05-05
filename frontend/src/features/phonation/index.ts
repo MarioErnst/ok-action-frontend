098bd1800c29cf053c4c588a916e38f7ee7120fc
Dayan <dayancorvo@gmail.com>
2026-05-05 18:13:37 -0400

    Remove PhonationTestPage from phonation feature public exports

--- frontend/src/features/phonation/index.ts
+++ frontend/src/features/phonation/index.ts
@@ -1,7 +1,6 @@
 export { default as useVoiceMonitor } from './hooks/useVoiceMonitor';
 export { default as useEvaluationSession } from './hooks/useEvaluationSession';
 export { default as PhonationDisplay } from './components/organisms/PhonationDisplay';
-export { default as PhonationTestPage } from './test/PhonationTestPage';
 export { default as EvaluationPage } from './pages/EvaluationPage';
 export { EvaluationMenu } from './components/organisms/EvaluationMenu';
 export { EvaluationScreen } from './components/organisms/EvaluationScreen';
